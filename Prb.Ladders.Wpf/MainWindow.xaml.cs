3858f233bca8087add7bbaaca49133720cf501ae
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2021-03-18 09:33:51 +0100

    Add project files.

---
+++ Prb.Ladders.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Linq;
+namespace Prb.Ladders.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+        private void lstLadders_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void cmbBrand_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void rdbAll_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+        private void rdb1_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+        private void rdb2_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+        private void rdb3_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+        private void btnSale_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
